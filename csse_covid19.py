75886e91e433258a5a163b756ce8cb6d19a48fa2
Sylvia Schmitt <ai.two.ys@gmail.com>
2020-03-24 11:09:31 +0100

    Interactive plots
    Interactive plots for CSSE Covid19 data time series

---
+++ csse_covid19.py
@@ -0,0 +1,196 @@
+'''
+Using the John Hopkins CSSE COVID-19 data:
+https://github.com/CSSEGISandData/COVID-19
+
+Using list of countries by population from Wikipedia:
+'https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)'
+'''
+
+import pandas as pd
+import re
+
+import altair as alt
+import pandas as pd
+import numpy as np
+import datetime
+
+#import ipywidgets as widgets
+#from IPython.display import display
+#from ipywidgets import Checkbox, HBox, VBox
+#from IPython.display import clear_output
+
+from enum import Enum
+
+class Covid19_status(Enum):
+  Confirmed=(0)
+  Deaths=(1)
+  Recovered=(2)
+  Active=(3)
+  Merged=(4)
+
+  def __init__(self, idx):
+    self.csse = None
+
+class Csse_covid19(Enum):
+  Confirmed = (Covid19_status.Confirmed, 
+               'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv')
+  Deaths = (Covid19_status.Deaths, 
+            'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv')
+  Recovered = (Covid19_status.Recovered,
+               'https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
+  Active = (Covid19_status.Active, None)
+  Merged = (Covid19_status.Merged, None)
+
+  def __init__(self, status, url):
+    self.status = status
+    self.url = url
+    self.status.csse = self
+    self.df_raw = None
+    self.df_pop = None 
+    self.re_date='^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{1,2}$'    
+    if self.status == Covid19_status.Active:
+      self.df_raw = self.Confirmed.df_raw.copy()
+      self.date_cols = self.df_raw.filter(regex=self.re_date).columns.values 
+      self.df_raw[self.date_cols] = self.df_raw[self.date_cols].sub(
+          self.Recovered.df_raw[self.date_cols]).sub(
+              self.Deaths.df_raw[self.date_cols])
+    elif self.status is Covid19_status.Merged:
+      cols = self.Confirmed.df_raw.columns.values.tolist()
+      cols.insert(0,'Status')
+      self.df_raw = pd.DataFrame(columns=cols)
+      for status in Covid19_status:
+        if status is Covid19_status.Merged:
+          continue
+        df = status.csse.df_raw.copy()
+        df['Status'] = status.name
+        self.df_raw = self.df_raw.append(df, sort=False, ignore_index=True)      
+    else:
+      self.df_raw = pd.read_csv(self.url)
+      self.date_cols = self.df_raw.filter(regex=self.re_date).columns.values
+    
+  @staticmethod
+  def get_names():
+    return [status.name for status in Csse_covid19]
+
+  @staticmethod
+  def add_population(csv_merged_countries_population): 
+    df_countries_population = pd.read_csv(csv_merged_countries_population, header=0, sep=';')
+    df_countries_population.dropna(inplace=True)
+    df_countries_population['Country or area'].str.replace('\[[a-z0-9]\]$', '', regex=True)
+    for status in Csse_covid19:
+      status.__add_population(df_countries_population)
+
+  def __add_population(self, df_countries_population): 
+    for index, row in df_countries_population.iterrows():
+      country_csse = row['Csse Covid-19 countries']      
+      if country_csse != np.NaN:
+        count = row['Population(1 July 2019)']
+        self.df_raw.loc[
+          self.df_raw['Country/Region']==country_csse, 
+          'Population(1 July 2019)'] = count
+
+  def alt_plot(self, countries=None, width=800, height=600, 
+               sort_legend=False, log_scale=None, 
+               normalize_by_population=False,
+               file_path=None, display=True, day_delta=0):
+    if 'Population(1 July 2019)' not in self.df_raw.columns:
+      Csse_covid19.add_population('merged_countries_population_nan.csv')      
+    source = self.df_raw
+    if countries is not None:
+      source = source.loc[source['Country/Region'].isin(countries)]
+    else:
+      countries = source['Country/Region'].values.tolist()
+
+    source = source.groupby('Country/Region').sum().reset_index()
+    source = source.set_index([source.index, 'Country/Region', 'Lat', 'Long', 'Population(1 July 2019)'])
+    source = source.stack()
+    source.index.set_names('Date', level=len(source.index.names)-1, inplace=True)
+    source = source.reset_index().rename(columns={0:'Count'})
+    if normalize_by_population:
+      source['Count'] /= source['Population(1 July 2019)'] * 1e-6
+
+    first_date_str = source.iloc[0]['Date'] 
+    last_date_str = source.iloc[-1]['Date']
+    first_date = datetime.datetime.strptime(first_date_str, "%m/%d/%y")
+    last_date = datetime.datetime.strptime(last_date_str, "%m/%d/%y")
+    end_date_str = (last_date  + datetime.timedelta(days=day_delta)).strftime("%m/%d/%y")
+    date_range=[first_date_str, end_date_str]
+    alt.data_transformers.disable_max_rows()
+    # Create a selection that chooses the nearest point & selects based on x-value
+    nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                            fields=['Date'], empty='none')
+
+    scale = alt.Scale(type='linear')
+    color = alt.Color('Country/Region')
+    log_threshold = -1
+    sort = countries
+    title = '{} - including {}'.format(self.name, datetime.datetime.strftime(last_date, '%Y-%m-%d'))
+    title_y = 'Count'
+    if log_scale != None:
+      scale = alt.Scale(type="log", base=log_scale)  
+      log_threshold = 0
+    if sort_legend:
+      sort = self.get_countries_sorted(countries)
+    if normalize_by_population:
+      title_y = 'Count per million'
+      title += ', normalized per million of the population'
+    color = alt.Color('Country/Region', sort=sort)                    
+  
+    line = alt.Chart(source).transform_filter( 
+        alt.datum.Count > log_threshold).mark_line().encode(
+        alt.X('Date:T', scale=alt.Scale(domain=date_range), axis=alt.Axis(format = ("%d %b %Y"), labelAngle=90)), 
+        alt.Y('Count:Q', scale=scale, axis=alt.Axis(orient='right', title=title_y)), 
+        color
+        ).properties(title=title)#.interactive()
+    # Transparent selectors across the chart. This is what tells us
+    # the x-value of the cursor
+    selectors = alt.Chart(source).mark_point().encode(
+        x='Date:T', 
+        opacity=alt.value(0),
+    ).add_selection(nearest)
+    # Draw points on the line, and highlight based on selection
+    points = line.mark_point().encode(
+        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+    ).interactive()
+    # Draw text labels near the points, and highlight based on selection
+    text = line.mark_text(align='right', dx=-5, dy=-5).encode(
+        text=alt.condition(nearest, 'Count:Q', alt.value(' '))
+    )
+    # Draw a rule at the location of the selection
+    rules = alt.Chart(source).mark_rule(color='gray').encode(
+        x='Date:T',
+    ).transform_filter(nearest)
+    # Put the five layers into a chart and bind the data
+    layer = alt.layer(line, 
+                      selectors, 
+                      points, rules, text,
+    ).properties(width=width, height=height)    
+
+    if file_path is not None:
+      layer.save(file_path)
+    if display:
+      layer.display()
+    return #layer #layer.display()
+
+  def get_top_countries(self, top=10):
+    date = self.df_raw.filter(regex=self.re_date).columns.values[-1]
+    df = self.df_raw.groupby(by='Country/Region').sum()
+    df.sort_values(by=[date], inplace=True, ascending=False)
+    df = df.iloc[:top]
+    return df.index.values.tolist()
+
+  def get_countries_sorted(self, countries):
+    date = self.df_raw.filter(regex=self.re_date).columns.values[-1]
+    df = self.df_raw.groupby(by='Country/Region').sum()
+    df.sort_values(by=[date], inplace=True, ascending=False)
+    return df.iloc[df.index.isin(countries)].index.tolist()
+
+  def get_by_country(self, countries=None, plot=False, figsize=(10,10)):
+    df = self.df_raw.groupby(by='Country/Region').sum().filter(self.date_cols)
+    if countries is None:
+      df = df.transpose()
+    else:
+      df = df.loc[countries].transpose()    
+    if plot:
+      fig = df.plot(figsize=figsize)
+    return df, fig
---
+++ export_csv_files.py
@@ -0,0 +1,51 @@
+from csse_covid19 import Csse_covid19
+
+import pandas as pd
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_world_population_from_wiki():
+    url = 'https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)'
+    page = requests.get(url)
+    html = BeautifulSoup(page.content, 'html.parser')
+    element = html.find(text=re.compile('Country or area'))
+    #element = html.find(text=re.compile('Countries and areas ranked by population in 2019'))
+    html_population_table = element.findParent('table')
+    df_population = pd.read_html(str(html_population_table))[0]
+    df_population['Country or area'] = df_population['Country or area'].str.replace('\[[a-z0-9]\]$', '', regex=True)
+    df_population.sort_values(by='Country or area', inplace=True)
+    return df_population.loc[:, ['Country or area', 'Population(1 July 2019)']]
+
+def get_csse_covid19_countries():
+    countries = Csse_covid19.Active.df_raw.groupby(by=['Country/Region']).sum().index.tolist()   
+    df_countries = pd.DataFrame(columns=['Csse Covid-19 countries'], data=countries)
+    df_countries.sort_values(by='Csse Covid-19 countries', inplace=True)
+    return df_countries
+
+def export_csv():
+    get_world_population_from_wiki().to_csv('world_population_by_countries.csv')
+    get_csse_covid19_countries().to_csv('csse_covid19_countries.csv')
+
+export_csv()
+#def merge_world_population_csse_countries(merged_csv_path):
+#    df_countries = pd.read_csv(merged_csv_path)
+#    df_countries.dropna(inplace=True)
+#    df_countries['population_countries'].str.replace('\[[a-z0-9]\]$', '', regex=True)
+#    
+#    df_population.sort_values(by=['Country or area'], inplace=True)
+#    df_countries.sort_values(by=['population_countries'], inplace=True)
+#
+#    for index, row in df_countries.iterrows():
+#        country_csse = row['csse_covid19_countries']
+#        country_pop = row['population_countries']
+#        print(country_csse, country_pop)
+#        #if False:
+#        if country_csse != np.NaN:
+#          count = df_population.loc[df_population['Country or area'] == country_pop,
+#                                    'Population(1 July 2019)'].values
+#        #df = csse_covid19.df_raw
+#        #df.loc[df['Country/Region']==country_csse, 
+#        #          'Population_2019-7-1'] = count
+#        df_countries.loc[index, 'Population_2019_7_1'] = count   
