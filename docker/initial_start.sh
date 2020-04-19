58a01cba128958b2e2b5b4f81287ca88332a0fc7
Sylvia Schmitt <ai.two.ys@gmail.com>
2020-04-19 18:01:36 +0200

    Update plots 18 April 2020

--- docker/initial_start.sh
+++ docker/initial_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #adjust to your needs
-targetport="8888"
+targetport="8887"
 containername="covid19_eda"
 #------------------------------------
 imagename="ai2ys/jupyterlab:covid19eda"
@@ -18,10 +18,14 @@ fi
 cd `dirname $0`
 
 docker run \
-    --user $(id -u $user):$(id -g $user) \
+    --rm \
+    --tty \
+    --user $USER \
     --env USER=${user} \
     --name ${containername} \
     --workdir=${workdir} \
     --volume $(realpath ../):${workdir}/ \
     --publish=8888:${targetport} \
     ${imagename}
+
+    #--user $(id -u $user):$(id -g $user) \
